3ef74b6ed1e22aa68fff563c2fefbf02a7ce394a
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-19 19:21:53 +0000

    Updated layout.tsx file

--- prodev-mobile-app-0x05/app/(home)/_layout.tsx
+++ prodev-mobile-app-0x05/app/(home)/_layout.tsx
@@ -4,8 +4,8 @@ import { Tabs } from "expo-router";
 const HomeRootLayout = () => {
   return (
     <Tabs screenOptions={{
-        tabBarActiveTintColor: '#34967C',
-        headerShown: false
+      tabBarActiveTintColor: '#34967C',
+      headerShown: false
     }} >
       <Tabs.Screen name="index" options={{
         title: 'Home',
@@ -21,15 +21,15 @@ const HomeRootLayout = () => {
         headerShown: true,
         tabBarIcon: ({ color }) => <EvilIcons name="heart" size={27} color={color} />
       }} />
-        <Tabs.Screen name="inbox" options={{
+      <Tabs.Screen name="inbox" options={{
         title: 'Inbox',
         headerShown: true,
         tabBarIcon: ({ color }) => <Ionicons name="chatbubbles-outline" size={24} color={color} />
       }} />
-        <Tabs.Screen name="profile" options={{
+      <Tabs.Screen name="profile" options={{
         title: 'Profile',
         headerShown: true,
-        tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color="black" />
+        tabBarIcon: ({ color }) => <FontAwesome name="user-o" size={24} color={color} />
       }} />
     </Tabs>
   )
