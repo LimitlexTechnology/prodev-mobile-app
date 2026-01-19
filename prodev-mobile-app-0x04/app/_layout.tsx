9318a70efa9e4ac079b7a8cf709a7b141e4c4d0f
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-19 19:17:51 +0000

    Updated layout.tsx

--- prodev-mobile-app-0x04/app/_layout.tsx
+++ prodev-mobile-app-0x04/app/_layout.tsx
@@ -8,6 +8,6 @@ export default function RootLayout() {
       <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
-    </Stack>
+      </Stack>
   );
 }
