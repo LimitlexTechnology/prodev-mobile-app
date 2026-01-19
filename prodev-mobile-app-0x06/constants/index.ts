5051790723819a30754a028406bb2997a3a395b2
Limitlex Technologies <kobbygazi@gmail.com>
2026-01-19 21:53:04 +0000

    Updated Index

--- prodev-mobile-app-0x06/constants/index.ts
+++ prodev-mobile-app-0x06/constants/index.ts
@@ -1,6 +1,6 @@
-const BACKGROUNDIMAGE = require('@/assets/images/hero-icon.png')
+const BACKGROUNDIMAGE = require('@/assets/images/hero-icon.jpg')
 const HEROLOGO = require('@/assets/images/logo.png')
-const HEROLOGOGREEN = require("@/assets/images/logo-green.png")
+const HEROLOGOGREEN = require("@/assets/images/logo-green.jpg")
 const GOOGLELOGO = require('@/assets/images/google.png')
 const FACEBOOKLOGO = require('@/assets/images/facebook.png')
 
